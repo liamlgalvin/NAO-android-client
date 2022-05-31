547e9e66df70fd764f91f9ff33ba7387054044cc
Liam Galvin <liamlgalvin@gmail.com>
2022-05-31 13:17:24 +0200

    change top app bar background colour

--- src/main/java/com/example/naoandroidclient/ui/main/topbar/MainAppBar.kt
+++ src/main/java/com/example/naoandroidclient/ui/main/topbar/MainAppBar.kt
@@ -10,10 +10,12 @@ import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -34,6 +36,8 @@ fun MainAppBar(
     val currentBackStackEntry by navController.currentBackStackEntryAsState()
 
     TopAppBar(
+       elevation = 0.dp,
+        backgroundColor = MaterialTheme.colorScheme.background,
         title = {
                 Text(text = stringResource(id = R.string.app_name))
                 },
